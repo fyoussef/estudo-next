a2cbdf1edcdfeb9db0c8ca240c4dca26dc72f767
fyoussef <fillipyuou@gmail.com>
2022-02-05 21:36:50 -0300

    Index alterdo, e pagina para o primeiro post adicionada

---
+++ pages/post1.js
@@ -0,0 +1,16 @@
+
+import Header from '../components/Header';
+import HomeSection from '../components/HomeSection'
+
+function Post1() {
+    return(
+        <div>
+            <Header/>
+
+            <HomeSection text={''} />
+        </div>
+        
+    )
+}
+
+export default Post1
