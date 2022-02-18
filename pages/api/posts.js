97778bed24281e7b6d932006f8000cbb454ab3c5
Filipi Youssef <filipi.youssef1@gmail.com>
2022-02-18 16:28:00 -0300

    Atualiando

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,7 +1,12 @@
 
 import posts from '../../json/posts.json'
+import connectDB from '../../utils/connection'
 
-export default function getPosts(req, res) {
+export default async function getPosts(req, res) {
+
+    const { db } = await connectDB()
+
+    const response = db.collection("blog")
 
     res.status(200).json(posts)
 
