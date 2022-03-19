464703c95363416767c89e07c6e16f79fdfce8ef
fyoussef <fillipyuou@gmail.com>
2022-03-19 17:25:16 -0300

    :sparkles: Novo modal adicionado e novos componentes para icones SVG

---
+++ components/SVGIcons.js
@@ -0,0 +1,29 @@
+export function CheckedIcon () {
+    return (
+        <svg className="mx-auto mb-4 w-14 h-14 text-green-500 dark:text-green-400" 
+             fill="none" 
+             stroke="currentColor" 
+             viewBox="0 0 24 24" 
+             xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        </svg>
+    )
+}
+
+export function AtentionIcon() {
+    return (
+        <svg className="mx-auto mb-4 w-14 h-14 text-red-600 dark:text-red-600" 
+             fill="none" 
+             stroke="currentColor" 
+             viewBox="0 0 24 24" 
+             xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+        </svg>
+    )
+}
+
+export function CloseIcon() {
+    return (
+        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+    )
+}
